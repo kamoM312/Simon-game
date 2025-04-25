e339a00ed0b3bb8034b2666a6beb8b680c6501d1
kamo312 <kamogelomalema1@gmail.com>
2025-04-25 16:58:51 -0400

    completed step 10

--- game.js
+++ game.js
@@ -4,6 +4,8 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 
+var started = false;
+
 function nextSequence() {
     level++;
     $("h1").text("Level "+level);
@@ -22,28 +24,16 @@ function nextSequence() {
     playSound(randomChosenColour);
 }
 
-
-
 $(".btn").on( "click", function() {
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
     playSound(userChosenColour);
     animatePress(userChosenColour);
     console.log(userChosenColour);
-    
-    // if(userClickedPattern.length === gamePattern.length) {
-    //     checkAnswer(userClickedPattern[userClickedPattern.length - 1]);
-        
-    //     setTimeout(function() {
-    //         userClickedPattern = [];
-    //         nextSequence();
-    //     }, 1000);
+    checkAnswer(userClickedPattern[userClickedPattern.length - 1]); 
+}
 
-    // } else {
-        checkAnswer(userClickedPattern[userClickedPattern.length - 1]); 
-    }
-    
- );
+);
 
 function playSound(name) {
     var audio = new Audio(`./sounds/${name}.mp3`);
@@ -59,9 +49,12 @@ function animatePress(currentColour) {
     }, 100);
 }
 
-$(document).one("keydown", function() {
-    $("h1").text("Level "+level);
-    nextSequence();
+$(document).on("keydown", function() {
+    if(started === false) {
+       $("h1").text("Level "+level);
+        nextSequence(); 
+        started = true; 
+    }  
 });
 
 var level = 0;
@@ -88,8 +81,15 @@ function checkAnswer(currentLevel) {
             $(document.body).removeClass("game-over");
         }, 200);
         $("h1").text("Game Over, Press Any Key to Restart");
+        startOver();
     }
 
     console.log("user: " + userClickedPattern);
     console.log("game: " + gamePattern);
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
 }
